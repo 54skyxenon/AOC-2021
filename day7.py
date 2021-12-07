54ed0cd9657e1b90c5df6ccc3f64a8b3a9dc0aec
Brandon Liang <54skyxenon@gmail.com>
2021-12-07 15:46:01 +0100

    Finish day 7

---
+++ day7.py
@@ -0,0 +1,24 @@
+from typing import List
+
+def summation(x : int) -> int:
+    ''' Closed form formula for summation. '''
+    return (x * (x + 1)) // 2
+
+def part1(xs : List[int]) -> int:
+    ''' Solve part 1 '''
+    left, right = min(xs), max(xs)
+    return min(sum(abs(x - i) for x in xs) for i in range(left, right + 1))
+
+def part2(xs : List[int]) -> int:
+    ''' Solve part 2 '''
+    left, right = min(xs), max(xs)
+    return min(sum(summation(abs(x - i)) for x in xs) for i in range(left, right + 1))
+
+with open('day7.txt') as f:
+    lines = list(map(int, f.readlines()[0].rstrip('\n').split(',')))
+    print(part1(lines))
+    print(part2(lines))
+
+example_input = [16,1,2,0,4,2,7,1,2,14]
+assert part1(example_input) == 37
+assert part2(example_input) == 168
