fcb6f4ce962d4e53755545cfdc56f7292d9bdc3a
Brandon Liang <54skyxenon@gmail.com>
2021-12-24 02:15:46 +0100

    Finish day 23, god bless Noble

--- day23.py
+++ day23.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Tuple
+from typing import List, Tuple
 from collections import defaultdict
 from heapdict import heapdict
 
@@ -6,6 +6,7 @@ Grid = List[List[str]]
 Edge = Tuple[int, str]
 
 ENERGY_OF_TYPE = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
+HALLWAY_INDEX = {'A': 3, 'B': 5, 'C': 7, 'D': 9}
 
 def flatten(grid : Grid) -> str:
     ''' Flatten a grid into just a string (to be hashed). '''
@@ -16,6 +17,7 @@ def unflatten(grid_str : str) -> Grid:
     as_grid = []
     for i in range(0, len(grid_str), 13):
         as_grid.append(grid_str[i:i + 13])
+    
     return as_grid
 
 def set_grid(from_i : int, from_j : int, i : int, j : int, val : str, grid : Grid) -> Grid:
@@ -25,8 +27,61 @@ def set_grid(from_i : int, from_j : int, i : int, j : int, val : str, grid : Gri
     new_grid[i][j] = val
     return [''.join(row) for row in new_grid]
 
-def dijkstra(get_neis : Callable[[Grid], List[Edge]], start : str, end : str) -> int:
-    ''' Dijkstra's with a heap - O(n log n + m log n) time '''
+def get_neis(grid : Grid, height : int) -> List[Edge]:
+    ''' Simulate all possible next states from an amphipod move on a grid with a given hallway height. '''
+    neis = []
+
+    ## get all amphipods that are not in the right place
+    amphipods = []
+    for r in range(1, len(grid) - 1):
+        for c in range(1, len(grid[0]) - 1):
+            if grid[r][c] in HALLWAY_INDEX:
+                amphipod = grid[r][c]
+                hall = HALLWAY_INDEX[amphipod]
+                if r == 1 or not (c == hall and all(grid[rr][hall] == amphipod for rr in range(r, height + 1))):
+                    amphipods.append((amphipod, r, c))
+    
+    for amphipod, r, c in amphipods:
+        def try_move(cost : int, nr : int, nc : int) -> Edge:
+            ''' Try a valid move and add it as an edge in a weighted graph of states. '''
+            after_move = set_grid(r, c, nr, nc, amphipod, grid)
+            return (cost, flatten(after_move))
+        
+        ## try moving amphipods on first row into a hallway
+        if r == 1:
+            hall = HALLWAY_INDEX[amphipod]
+            if (c < hall and all(grid[r][cc] == '.' for cc in range(c + 1, hall + 1))) or\
+                (c > hall and all(grid[r][cc] == '.' for cc in range(c - 1, hall - 1, -1))):
+                hallway_state = ''.join(grid[rr][hall] for rr in range(2, height + 1))
+                acceptable_states = {'.' * (height - 1)}
+                for i in range(1, height - 1):
+                    acceptable_states.add('.' * i + amphipod * (height - 1 - i))
+
+                if hallway_state in acceptable_states:
+                    moves = abs(hall - c) + hallway_state.count('.')
+                    neis.append(try_move(
+                        moves * ENERGY_OF_TYPE[amphipod],
+                        hallway_state.count('.') + 1,
+                        hall))
+        
+        ## need to try moving amphipods out of hallway rows
+        elif 2 <= r <= height and all(grid[rr][c] == '.' for rr in range(1, r)):
+            for cc in range(c - 1, 0, -1):
+                if grid[1][cc] != '.':
+                    break
+                if cc not in HALLWAY_INDEX.values() and grid[1][cc] == '.':
+                    neis.append(try_move((r - 1 + abs(cc - c)) * ENERGY_OF_TYPE[amphipod], 1, cc))
+            
+            for cc in range(c + 1, len(grid[1])):
+                if grid[1][cc] != '.':
+                    break
+                if cc not in HALLWAY_INDEX.values() and grid[1][cc] == '.':
+                    neis.append(try_move((r - 1 + abs(cc - c)) * ENERGY_OF_TYPE[amphipod], 1, cc))
+    
+    return neis
+
+def dijkstra(height : int, start : str, end : str) -> int:
+    ''' Dijkstra's algorithm without a graph, just finding neighbors until we reach the end with least cost! '''
     dists = defaultdict(lambda: float('inf'))
     dists[start] = 0
     
@@ -38,96 +93,18 @@ def dijkstra(get_neis : Callable[[Grid], List[Edge]], start : str, end : str) ->
     while True:
         # extract min
         u, dists[u] = Q.popitem()
-        print('at dist =', dists[u])
         visited.add(u)
         if u == end:
             return dists[u]
         
         # consider outneighbors
-        for cost, v in get_neis(unflatten(u)):
+        for cost, v in get_neis(unflatten(u), height):
             if v not in visited:
                 if dists[v] > dists[u] + cost:
                     Q[v] = dists[v] = dists[u] + cost
 
 def part1(grid : Grid) -> int:
     ''' Solve part 1 '''
-    
-    def get_neis_p1(grid : Grid) -> List[Edge]:
-        ''' Simulate all possible next states from an amphipod move. '''
-        neis = []
-
-        ## get all amphipods that are not in the right place
-        amphipods = []
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == 'A':
-                    if (r, c) != (3, 3) and not ((r, c) == (2, 3) and grid[3][3] == 'A'):
-                        amphipods.append(('A', r, c))
-                elif grid[r][c] == 'B':
-                    if (r, c) != (3, 5) and not ((r, c) == (2, 5) and grid[3][5] == 'B'):
-                        amphipods.append(('B', r, c))
-                elif grid[r][c] == 'C':
-                    if (r, c) != (3, 7) and not ((r, c) == (2, 7) and grid[3][7] == 'C'):
-                        amphipods.append(('C', r, c))
-                elif grid[r][c] == 'D':
-                    if (r, c) != (3, 9) and not ((r, c) == (2, 9) and grid[3][9] == 'D'):
-                        amphipods.append(('D', r, c))
-
-        for type, r, c in amphipods:
-            def try_move(cost : int, nr : int, nc : int) -> Edge:
-                ''' Try a valid move and add it as an edge in a weighted graph of states. '''
-                after_move = set_grid(r, c, nr, nc, type, grid)
-                return (cost, flatten(after_move))
-            
-            ## move amphipods on first row left or right, or into a hall if it matches theirs
-            if r == 1:
-                if type == 'A' and c in {2, 4}:
-                    if grid[1][3] == '.':
-                        if grid[2][3] + grid[3][3] == '..':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 3, 3, 3))
-                        if grid[2][3] + grid[3][3] == '.A':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 2, 2, 3))
-                
-                if type == 'B' and c in {4, 6}:
-                    if grid[1][5] == '.':
-                        if grid[2][5] + grid[3][5] == '..':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 3, 3, 5))
-                        if grid[2][5] + grid[3][5] == '.B':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 2, 2, 5))
-                
-                if type == 'C' and c in {6, 8}:
-                    if grid[1][7] == '.':
-                        if grid[2][7] + grid[3][7] == '..':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 3, 3, 7))
-                        if grid[2][7] + grid[3][7] == '.C':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 2, 2, 7))
-                
-                if type == 'D' and c in {8, 10}:
-                    if grid[1][9] == '.':
-                        if grid[2][9] + grid[3][9] == '..':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 3, 3, 9))
-                        if grid[2][9] + grid[3][9] == '.D':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 2, 2, 9))
-                
-                for nr, nc in [(r, c + 1), (r, c - 1)]:
-                    if nc not in {3, 5, 7, 9} and 0 <= nr < len(grid) and 0 <= nc < len(grid[nr]) and grid[nr][nc] == '.':
-                        neis.append(try_move(ENERGY_OF_TYPE[type], nr, nc))
-
-                for nr, nc in [(r, c + 2), (r, c - 2)]:
-                    if nc not in {3, 5, 7, 9} and 0 <= nr < len(grid) and 0 <= nc < len(grid[nr]):
-                        if grid[nr][nc] == grid[nr][(c + nc) // 2] == '.':
-                            neis.append(try_move(ENERGY_OF_TYPE[type] * 2, nr, nc))
-
-            ## need to try moving out of second-third rows
-            elif r in {2, 3}:
-                if all(grid[rr][c] == '.' for rr in range(1, r)):
-                    if grid[1][c - 1] == '.':
-                        neis.append(try_move(ENERGY_OF_TYPE[type] * r, 1, c - 1))
-                    if grid[1][c + 1] == '.':
-                        neis.append(try_move(ENERGY_OF_TYPE[type] * r, 1, c + 1))
-
-        return neis
-    
     grid_done = [
         '#############',
         '#...........#',
@@ -135,126 +112,12 @@ def part1(grid : Grid) -> int:
         '  #A#B#C#D#  ',
         '  #########  '
     ]
-
-    return dijkstra(get_neis_p1, flatten(grid), flatten(grid_done))
+    return dijkstra(3, flatten(grid), flatten(grid_done))
 
 def part2(grid : Grid) -> int:
     ''' Solve part 1 '''
     grid.insert(3, '  #D#C#B#A#  ')
     grid.insert(4, '  #D#B#A#C#  ')
-
-    def get_neis_p2(grid : Grid) -> List[Edge]:
-        ''' Simulate all possible next states from an amphipod move. '''
-        neis = []
-
-        '''
-        #############
-        #...........#
-        ###B#C#B#D###
-          #D#C#B#A#
-          #D#B#A#C#
-          #A#D#C#A#
-          #########
-        '''
-
-        ## get all amphipods that are not in the right place
-        amphipods = []
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == 'A':
-                    if (r, c) != (5, 3) and\
-                    not ((r, c) == (4, 3) and grid[5][3] == 'A') and\
-                    not ((r, c) == (3, 3) and grid[4][3] + grid[5][3] == 'AA') and\
-                    not ((r, c) == (2, 3) and grid[3][3] + grid[4][3] + grid[5][3] == 'AAA'):
-                        amphipods.append(('A', r, c))
-                elif grid[r][c] == 'B':
-                    if (r, c) != (5, 5) and\
-                    not ((r, c) == (4, 5) and grid[5][5] == 'B') and\
-                    not ((r, c) == (3, 5) and grid[4][5] + grid[5][5] == 'BB') and\
-                    not ((r, c) == (2, 5) and grid[3][5] + grid[4][5] + grid[5][5] == 'BBB'):
-                        amphipods.append(('B', r, c))
-                elif grid[r][c] == 'C':
-                    if (r, c) != (5, 7) and\
-                    not ((r, c) == (4, 7) and grid[5][7] == 'C') and\
-                    not ((r, c) == (3, 7) and grid[4][7] + grid[5][7] == 'CC') and\
-                    not ((r, c) == (2, 7) and grid[3][7] + grid[4][7] + grid[5][7] == 'CCC'):
-                        amphipods.append(('C', r, c))
-                elif grid[r][c] == 'D':
-                    if (r, c) != (5, 9) and\
-                    not ((r, c) == (4, 9) and grid[5][9] == 'D') and\
-                    not ((r, c) == (3, 9) and grid[4][9] + grid[5][9] == 'DD') and\
-                    not ((r, c) == (2, 9) and grid[3][9] + grid[4][9] + grid[5][9] == 'DDD'):
-                        amphipods.append(('D', r, c))
-
-        for type, r, c in amphipods:
-            def try_move(cost : int, nr : int, nc : int) -> Edge:
-                ''' Try a valid move and make a new edge in a weighted graph of states. '''
-                after_move = set_grid(r, c, nr, nc, type, grid)
-                return (cost, flatten(after_move))
-
-            ## move amphipods on first row left or right, or into a hall if it matches theirs
-            if r == 1:
-                if type == 'A' and c in {2, 4}:
-                    if grid[1][3] == '.':
-                        if grid[2][3] + grid[3][3] + grid[4][3] + grid[5][3] == '....':
-                            neis.append(try_move(5 * ENERGY_OF_TYPE[type], 5, 3))
-                        if grid[2][3] + grid[3][3] + grid[4][3] + grid[5][3] == '...A':
-                            neis.append(try_move(4 * ENERGY_OF_TYPE[type], 4, 3))
-                        if grid[2][3] + grid[3][3] + grid[4][3] + grid[5][3] == '..AA':
-                            neis.append(try_move(3 * ENERGY_OF_TYPE[type], 3, 3))
-                        if grid[2][3] + grid[3][3] + grid[4][3] + grid[5][3] == '.AAA':
-                            neis.append(try_move(2 * ENERGY_OF_TYPE[type], 2, 3))
-                
-                if type == 'B' and c in {4, 6}:
-                    if grid[1][5] == '.':
-                        if grid[2][5] + grid[3][5] + grid[4][5] + grid[5][5] == '....':
-                            neis.append(try_move(5 * ENERGY_OF_TYPE[type], 5, 5))
-                        if grid[2][5] + grid[3][5] + grid[4][5] + grid[5][5] == '...B':
-                            neis.append(try_move(4 * ENERGY_OF_TYPE[type], 4, 5))
-                        if grid[2][5] + grid[3][5] + grid[4][5] + grid[5][5] == '..BB':
-                            neis.append(try_move(3 * ENERGY_OF_TYPE[type], 3, 5))
-                        if grid[2][5] + grid[3][5] + grid[4][5] + grid[5][5] == '.BBB':
-                            neis.append(try_move(2 * ENERGY_OF_TYPE[type], 2, 5))
-
-                if type == 'C' and c in {6, 8}:
-                    if grid[1][7] == '.':
-                        if grid[2][7] + grid[3][7] + grid[4][7] + grid[5][7] == '....':
-                            neis.append(try_move(5 * ENERGY_OF_TYPE[type], 5, 7))
-                        if grid[2][7] + grid[3][7] + grid[4][7] + grid[5][7] == '...C':
-                            neis.append(try_move(4 * ENERGY_OF_TYPE[type], 4, 7))
-                        if grid[2][7] + grid[3][7] + grid[4][7] + grid[5][7] == '..CC':
-                            neis.append(try_move(3 * ENERGY_OF_TYPE[type], 3, 7))
-                        if grid[2][7] + grid[3][7] + grid[4][7] + grid[5][7] == '.CCC':
-                            neis.append(try_move(2 * ENERGY_OF_TYPE[type], 2, 7))
-
-                if type == 'D' and c in {8, 10}:
-                    if grid[1][9] == '.':
-                        if grid[2][9] + grid[3][9] + grid[4][9] + grid[5][9] == '....':
-                            neis.append(try_move(5 * ENERGY_OF_TYPE[type], 5, 9))
-                        if grid[2][9] + grid[3][9] + grid[4][9] + grid[5][9] == '...D':
-                            neis.append(try_move(4 * ENERGY_OF_TYPE[type], 4, 9))
-                        if grid[2][9] + grid[3][9] + grid[4][9] + grid[5][9] == '..DD':
-                            neis.append(try_move(3 * ENERGY_OF_TYPE[type], 3, 9))
-                        if grid[2][9] + grid[3][9] + grid[4][9] + grid[5][9] == '.DDD':
-                            neis.append(try_move(2 * ENERGY_OF_TYPE[type], 2, 9))
-                
-                for nr, nc in [(r, c + 1), (r, c - 1)]:
-                    if nc not in {3, 5, 7, 9} and 0 <= nr < len(grid) and 0 <= nc < len(grid[nr]) and grid[nr][nc] == '.':
-                        neis.append(try_move(ENERGY_OF_TYPE[type], nr, nc))
-
-                for nr, nc in [(r, c + 2), (r, c - 2)]:
-                    if nc not in {3, 5, 7, 9} and 0 <= nr < len(grid) and 0 <= nc < len(grid[nr]) and grid[nr][nc] == grid[nr][(c + nc) // 2] == '.':
-                        neis.append(try_move(2 * ENERGY_OF_TYPE[type], nr, nc))
-
-            ## need to try moving out of second-fifth rows
-            elif r in {2, 3, 4, 5} and all(grid[rr][c] == '.' for rr in range(1, r)):
-                if grid[1][c - 1] == '.':
-                    neis.append(try_move(r * ENERGY_OF_TYPE[type], 1, c - 1))
-                if grid[1][c + 1] == '.':
-                    neis.append(try_move(r * ENERGY_OF_TYPE[type], 1, c + 1))
-
-        return neis
-    
     grid_done = [
         '#############',
         '#...........#',
@@ -264,8 +127,7 @@ def part2(grid : Grid) -> int:
         '  #A#B#C#D#  ',
         '  #########  '
     ]
-
-    return dijkstra(get_neis_p2, flatten(grid), flatten(grid_done))
+    return dijkstra(5, flatten(grid), flatten(grid_done))
 
 with open('input/day23.txt') as f:
     grid = [line.rstrip('\n').ljust(13) for line in f.readlines()]
